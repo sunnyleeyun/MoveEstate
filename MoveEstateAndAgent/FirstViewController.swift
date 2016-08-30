efaa09b4053fdc2e23731d23b031094c085d6a1a
sunnyleeyun <sunnylee945@gmail.com>
2016-08-30 16:25:24 +0800

    Initial Commit

---
+++ MoveEstateAndAgent/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoveEstateAndAgent
+//
+//  Created by Sunny on 2016/8/30.
+//  Copyright © 2016年 Sunny. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
