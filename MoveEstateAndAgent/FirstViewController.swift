38ec756ba5a8548d3c6fafcfd4eaef88b7f451f7
sunnyleeyun <sunnylee945@gmail.com>
2016-08-31 18:48:05 +0800

    prototype

--- MoveEstateAndAgent/FirstViewController.swift
+++ MoveEstateAndAgent/FirstViewController.swift
@@ -2,24 +2,25 @@
 //  FirstViewController.swift
 //  MoveEstateAndAgent
 //
-//  Created by Sunny on 2016/8/30.
+//  Created by Sunny on 2016/8/31.
 //  Copyright © 2016年 Sunny. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var MoveLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.MoveLogo.image = UIImage(named: "MoveLogo")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
